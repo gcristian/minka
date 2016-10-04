c1e7a1bc1fa1eeb0f8e08ff78bcf555cbea4ba39
Cristian Gonzalez <cristian@flowics.com>
2016-10-04 02:45:45 -0300

    ascii art for logo

--- server/src/main/java/io/tilt/minka/utils/LogUtils.java
+++ server/src/main/java/io/tilt/minka/utils/LogUtils.java
@@ -23,6 +23,15 @@ import io.tilt.minka.domain.ShardID;
  * https://www.xatworld.com/nicks/letrasycaracteresxat.html
  */
 public class LogUtils {
+	
+		public final static String logo = "\n" +
+			"   ooo        ooooo  o8o              oooo                \n" +  
+			"   `88.       .888'  `\"'              `888                \n" + 
+			"    888b     d'888  oooo  ooo. .oo.    888  oooo   .oooo. \n" + 
+			"    8 Y88. .P  888  `888  `888P\"Y88b   888 .8P'   `P  )88b\n" + 
+			"    8  `888'   888   888   888   888   888888.     .oP\"888\n" + 
+			"    8    Y     888   888   888   888   888 `88b.  d8(  888\n" +
+			"   o8o        o888o o888o o888o o888o o888o o888o `Y888\"\"8o\n\n"; 
 
 		public final static char OK = '√';
 		public final static char CRASH = '⚡';
@@ -64,7 +73,7 @@ public class LogUtils {
 			StringBuilder sb = new StringBuilder(nl);
 			grossLine(LARGE * 2, sb);
 			sb.append(nl).append(nl);
-			sb.append("    ●       M I N K A       ● ").append(nl).append(nl);
+			sb.append(logo);
 			sb.append("    Distributing duties since 1438, where no follower has ever got").append(nl).append(nl);
 			sb.append("    Service: ").append(serviceName).append(nl);
 			sb.append("    ShardID: ").append(id).append(nl).append(nl);
