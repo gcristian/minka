7788cb0fe01a82e297d52a86fd21d22dd90c852c
Cristian Gonzalez <gcristian@gmail.com>
2018-02-22 12:53:00 -0300

    improved sampler

---
+++ samples/sampler.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# ------------------- Minka Demo launcher --------------------
+# scans for a non already used & busy port to configure minka 
+# broker, starting at port 9000, takes the first one free
+# ------------------------------------------------------------
+port_start=9000
+host='localhost'
+xms='512M'
+pp='broker.hostPort'
+dsfp=$(pwd)'/'${1:-'dataset-3pallets-fair-balancer-4shards'}
+another='false'
+for i in {0..20}; do
+	echo $dsfp
+	i=$(($i+$port_start))
+	x=`netstat -ltn | grep $i | grep -v grep`
+	y=`ps aux | grep "$pp=$host:$i"| grep -v 'grep'`
+	if [ -z "$x" ] && [ -z "$y" ]; then
+		echo "Using port: $i"
+		cmd="mvn -o "
+		if $another ; then cmd=$cmd" compile install -DskipTests "; fi
+		p=$p"-DXms$xms -DXmx$xms exec:java "
+		p=$p"-Dexec.mainClass=FileDatasetEmulator.main "
+		p=$p"-Dmins=1440 -D$pp=$host:$i "
+		p=$p"-Ddataset.filepath=$dsfp"
+		echo $cmd $p
+		$cmd $p
+		exit
+	else
+		another = true
+		echo "Port: $i busy (netstat got $x)"
+	fi
+done
+
